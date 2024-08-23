f8d5a58b9339b0b64c8b9ad5055333a6c7542ea0
Keazzz <k.britt135514@spartans.nsu.edu>
2024-08-23 00:40:21 -0400

    New Code

---
+++ New Line.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()  {
+    cout << "Hello World!!  \n"; //<<endl can also be used
+    cout << "I am learning C++ with the start new line print feature";
+
+    return 0; 
+}
