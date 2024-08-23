ca5a7aa2dd804a28689b250c767ea85974d92b59
Keazzz <k.britt135514@spartans.nsu.edu>
2024-08-23 00:42:22 -0400

    Update New Line.cpp

--- New Line.cpp
+++ New Line.cpp
@@ -6,5 +6,11 @@ int main()  {
     cout << "Hello World!!  \n"; //<<endl can also be used
     cout << "I am learning C++ with the start new line print feature";
 
+
+
+
+    cout <<"You can also use endl for this" <<endl;
+    cout <<"See?" <<endl;
+
     return 0; 
 }
