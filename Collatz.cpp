a00f2770770972687fbb219779313ac6e55c19cf
kbritt135514 <k.britt135514@spartans.nsu.edu>
2024-08-26 11:51:56 -0400

    Add files via upload

---
+++ Collatz.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+    int x;
+    int i;
+    cout << "Enter an integer please: "<< endl;
+    cin >> x;
+    if (x % 2 == 0) {
+        cout <<"This is a even integer: ";
+        cout << x; 
+    }
+    else {
+        cout<< "This is a odd integer: ";
+        cout << x;
+    }
+
+    
+    return 0;
+    }
+        
+  
+
+
+
