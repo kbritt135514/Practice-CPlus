f6b6c1c16a1556f324853c85d0809c4e065c031e
Keanu.Britt <k.britt135514@spartans.nsu.edu>
2024-10-27 08:33:04 -0400

    Send it

--- Personal Practice/Practice 1.cpp
+++ Personal Practice/Practice 1.cpp
@@ -12,23 +12,21 @@ using namespace std;
 float hoursWorked;
 char jobType;
 int workerNumber;
-float cashierPay, floorPay, storagePay, fittingPay, managerPay, employeePay;
 float wageMulti;
 string employeeName;
 char reviewChoice;
-int employeeChoice
+int employeeChoice;
 bool reviewLoop = false;
+float = employeePay;
 
 
-string names[];
-string job[];
-float employeePayout[];
 
-cashierPay = 13.00;
-floorPay = 15.50;
-storagePay = 14.00;
-fittingPay = 15.50;
-managerPay = 25.50;
+
+float cashierPay = 13.00;
+float floorPay = 15.50;
+float storagePay = 14.00;
+double fittingPay = 15.50;
+float managerPay = 25.50;
 
 
 int main() {
@@ -39,6 +37,10 @@ int main() {
     cout << "How many employee(s) are being paid this pay period?" <<endl;
     cin >> workerNumber;
 
+    string names[workerNumber];
+    string job[workerNumber];
+    float employeePayout[workerNumber];
+
 
     while (workerNumber > 0) {
         for (int i = 1; i <= workerNumber; i++) {
@@ -48,7 +50,7 @@ int main() {
             names[i-1] = employeeName;
         
             cout << "Please select the job type for worker "<< i << ": "<<endl;
-            cout << "A: Cashier\n B: Floor Associate\n C: Storage Associate\n D: Fitting Room Associate\n E: Manager"
+            cout << "A: Cashier\n B: Floor Associate\n C: Storage Associate\n D: Fitting Room Associate\n E: Manager"<<endl;
             cin >> jobType;
 
             switch (jobType) {
@@ -78,7 +80,7 @@ int main() {
 
                 case 'e':
                 case 'E': cout << "Managerial Position Selected. Hourly wage is $25.50/hr"<<endl;
-                    job[i-1] = "Manager"
+                    job[i-1] = "Manager";
                     wageMulti = managerPay;
                     break;
 
@@ -95,7 +97,7 @@ int main() {
                 employeePayout[i - 1] = employeePay;
 
                 cout << fixed << setprecision(2) <<"Employee #"<< i << "earned a total of $"<<employeePay<<endl;
-            while (hoursWorked >= 0);
+            } while (hoursWorked >= 0);
 
 
 
@@ -110,9 +112,8 @@ int main() {
         switch (reviewChoice) {
             case 'y':
             case 'Y':
-            case 'Yes':
-            case 'yes': 
 
+                
                 reviewLoop = true
                 cout << "Which employee would you like to review?: 1 - "<< workerNumber<<endl;
                 cin >> employeeChoice;
@@ -129,10 +130,9 @@ int main() {
 
                 case 'n':
                 case 'N':
-                case 'No':
-                case 'no':
+
             }          
-        
+        }
 
 
     }
