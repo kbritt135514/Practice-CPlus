be8f4388284c2d2c23b731acaee43f50b39ca9b3
kbritt135514 <k.britt135514@spartans.nsu.edu>
2024-11-04 23:34:49 +0000

    uppdates

--- Personal Projects/ClassTest.cpp
+++ Personal Projects/ClassTest.cpp
@@ -1,61 +1,111 @@
 #include <iostream>
 #include <iomanip>
 
-
-
-
-
+//Global Variable section
 int userVal;
-int counter;
-
-
-void getNum(int &a);
-
-void collatzConjecture(int &a);
+int counter = 0;
+char menuChoice;
 
 
 
+
+//Protoype section
+
+
+//displayMenu Function Protos
+char displayMenu();
+
+
+//Collatz Function Protos
+int  getNum();
+void collatzConjecture(int userVal);
+void displayGraphicalRepresentation(int value, int step);
+
+
+// Main function 
 int main(){
 
-getNum(userVal);
+    menuChoice = displayMenu();
 
-collatzConjecture(userVal);
+    switch (menuChoice) {
+        case 'a':
+        case 'A':   
+            userVal = getNum();
+            collatzConjecture(userVal);
+            break;
 
+        case 'b':
+        case 'B': 
+            break;
+    }
+    
+    return 0;
 
 }
 
 
 
-void getNum(int &a){
+
+
+// Function Definitions 
+
+//Main Menu function.
+
+char displayMenu(){ 
+    std::cout << "Please enter the corrosponding letter of the program you would like to use:"<< std::endl;
+    std::cout << "A: Collatz Conjecture" << std::endl;
+
+    while (!(std::cin >> menuChoice) || !isalpha(menuChoice)) {
+        std::cout << "Please enter a Valid upper case or lower case character. \nPlease try again." <<std::endl;
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+    }
+    return menuChoice;
+}
+
+
+//Collatz Conjecture Functions
+int getNum(){
+    int input;
     std::cout << "Please enter a positive integer: "<< std::endl;
 
-    while (!(std::cin >> userVal) || (userVal < 0)) {
+    while (!(std::cin >> input) || (input < 0)) {
         std::cout << "Please enter a Valid Positive integer. \nPlease try again." <<std::endl;
         std::cin.clear();
         std::cin.ignore(10000, '\n');
     }
 
+    return input;
+
 }
 
-
-
-void collatzConjecture(int &a){
-    int b = a;
+void collatzConjecture(int userVal){
+    int b = userVal;
 
     while (!(b == 1)){
+        displayGraphicalRepresentation(b, counter);
         if (b % 2 != 0) {
             b = ((3 * b) + 1);
-            std::cout << b << std::endl;
             counter++;
         }
 
         else if (b % 2 == 0) {
             b /= 2;
-            std::cout << b << std::endl;
             counter++;
         }
         
     }
     
-    std::cout << "This program ran a total of " << counter << " calculations." << std::endl;
+    std::cout << "This program ran a total of " << counter << " calculations to reach the number 1." << std::endl;
+}
+
+
+void displayGraphicalRepresentation(int value, int step){
+    std::cout << "Step " << step + 1 <<" [" <<value<< "]: ";
+
+    for (int i = 0; i < value && i < 50; i++) {  
+        std::cout << "#";
+    } 
+
+    std::cout << std::endl;
 }
