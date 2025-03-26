10480f6feaefaecdfd58d57c72d1d1e6bfb0bae5
kbritt135514 <k.britt135514@spartans.nsu.edu>
2025-03-26 09:38:29 -0400

    Add files via upload
    Update Die file

--- CSC-260/usingDieClass.cpp
+++ CSC-260/usingDieClass.cpp
@@ -3,6 +3,11 @@
 
 
 /*
+File Name: C:\temp\LabUseDieClassLucindaJackson.cpp
+Author: Lucinda A. Jackson, Linda Smith (note: if 2 work together)
+Course: CSC-260L-01
+Program Description: This program is designed to simluate the use of two die by using classes, declarations, public functions and return statements is multiple ways. Such as Counting how many matches
+                     , counting how many rolls until two 6's are matched, and counting how many time the sum of the two die rolls is even. The function "roll" simulate the die rolling. While the "getnum()" returns the int number currently face up on the die.
 1) 2 Objects are created
 2) die1, and die2
 3) The face value is 1
@@ -63,31 +68,44 @@ int main() {
 
    cout << "*** Beginning of Part II ***"<< endl;
 
-   int matchCounter = 0;
-   for (int i = 0; i < 2000; i++){
+
+
+   int matchCounter = 0; //initialized and assign 0 to a matchCounter
+   for (int i = 0; i < 2000; i++){ //loop for 2000 rolls
       die1.roll();
       die2.roll();
 
-      if (die1.getNum() == die2.getNum()){
+      if (die1.getNum() == die2.getNum()){ // if checks for matches and counts them
          matchCounter++;
       }
    }
-
-   cout << "The Die matched a total of " << matchCounter << " Times.\n" <<endl;
-
+   cout << "The Die matched a total of " << matchCounter << " Times.\n" <<endl; //print results
 
 
-   int rollsUntilMatchCounter = 0;
 
-   while (die1.getNum()!= die2.getNum()) {
-      rollsUntilMatchCounter++;
+   int rollsUntilMatchCounter = 0; //initialized and assign 0 to a rollsUntilMatchCounter
+   do {
       die1.roll();
       die2.roll();
+      
+      if (die1.getNum() && die2.getNum() == 6) { //if checks for matching 6's
+         rollsUntilMatchCounter++;
+      }
+   } while (die1.getNum()!= 6 || die2.getNum() != 6);
+   cout << "It took a total of " << rollsUntilMatchCounter << " rolls until both die rolled a 6\n" <<endl;//output results
+
+
+
+   int evenSumMatchCounter = 0;//initialized and assign 0 to a evenSumMatchCounter
+   for (int i = 0; i < 3000; i++) { //For loops 3000 rolls
+      die1.roll();
+      die2.roll();
+
+      if ((die1.getNum() + die2.getNum()) % 2 == 0) { // if checks for if the sum of the two rolls is even and counts when it is
+         evenSumMatchCounter++;
+      }
    }
-
-   cout << "It took a total of " << rollsUntilMatchCounter << " rolls until the die matched\n" <<endl;
-
-   
+   cout << "The Sum of the two die was even a total of " << evenSumMatchCounter << " Times.\n" <<endl;// Output results
 
 
    cout << "*** Ending of Part II ***"<< endl;
@@ -106,3 +124,5 @@ void Die::roll() {
 int Die::getNum() const {
     return num;
 }
+
+
